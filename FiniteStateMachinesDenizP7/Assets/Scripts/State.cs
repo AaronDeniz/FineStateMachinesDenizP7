ec8179066e6a7bb74e57fabcd358ed2eabc0e0d9
Aaron  Deniz <145504641+AaronDeniz@users.noreply.github.com>
2025-01-31 15:26:44 -0800

    day 2
    2:40

--- FiniteStateMachinesDenizP7/Assets/Scripts/State.cs
+++ FiniteStateMachinesDenizP7/Assets/Scripts/State.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem.Android;
 
 public class State
 {
@@ -53,6 +55,28 @@ public class State
         return this;
     }
 
+    public bool CanSeePlayer()
+    {
+        Vector3 direction = player.position - npc.transform.position;
+        float angle = Vector3.Angle(direction, npc.transform.forward);
+
+        if(direction.magnitude < visDist && angle < visAngle)
+        {
+            return true; ;
+        }
+        return false;
+    }
+
+    public bool CanAttackPlayer()
+    {
+        Vector3 direction = player.position - npc.transform.position;
+        if (direction.magnitude < shootDist)
+        {
+            return true;
+        }
+        return false;
+    }
+
 }
 
 public class Idle : State
@@ -76,7 +100,7 @@ public class Idle : State
             nextState = new Patrol(npc, agent, anim, player);
             stage = EVENT.EXIT;
         }
-        base.Update();
+       
     }
 
     public override void Exit()
@@ -85,5 +109,121 @@ public class Idle : State
             base.Exit();
     }
 }
+
+public class Patrol: State
+{
+    int currentIndex = -1;
+
+    public Patrol(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+                : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.PATROL;
+        agent.speed = 2;
+        agent.isStopped = false;
+    }
+
+    public override void Enter()
+    {
+        currentIndex = 0;
+        anim.SetTrigger("isWalking");
+        base.Enter();
+    }
+    public override void Update()
+    {
+        if(agent.remainingDistance < 1)
+        {
+            if (currentIndex >= GameEnvironment.Singleton.Checkpoints.Count - 1)
+                currentIndex = 0;
+            else
+                currentIndex++;
+
+            agent.SetDestination(GameEnvironment.Singleton.Checkpoints[currentIndex].transform.position);
+        }
+
+        
+    }
+    public override void Exit()
+    {
+        anim.ResetTrigger("isWalkiing");
+        base.Exit();
+    }
+}
+
+public class Pursue: State
+{
+    public Pursue(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+                : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.PURSUE;
+        agent.speed = 5;
+        agent.isStopped = false;
+    }
+
+    public override void Enter()
+    {
+        anim.SetTrigger("isrunning");
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        agent.SetDestination(player.position);
+        if (agent.hasPath)
+        {
+            if (CanAttackPlayer())
+            {
+                nextState = new Attack(npc, agent, anim, player);
+                stage = EVENT.EXIT;
+            }
+            else if (!CanSeePlayer())
+            {
+                nextState = new Patrol(npc, agent, anim, player);
+                stage = EVENT.EXIT;
+            }
+        }
+    }
+
+    public override void Exit()
+    {
+        anim.ResetTrigger("isRunning");
+        base.Exit();
+    }
+}
+
+public class Attack : State
+{
+    float rorationSpeed = 2.0f;
+    AudioSource shoot;
+    public Attack(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+                : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.ATTACK;
+        shoot = _npc.GetComponent<AudioSource>();
+     
+    }
+
+    public override void Enter()
+    {
+        anim.SetTrigger("isShooting");
+        agent.isStopped = true;
+        shoot.Play();
+        base.Enter();
+    }
+    public override void Update()
+    {
+        Vector3 direction = player.position - npc.transform.position;
+        float angle = Vector3.Angle(direction, npc.transform.forward);
+        direction.y = 0;
+
+        npc.transform.rotation = Quaternion.Slerp(npc.transform.rotation,
+                                            Quaternion.LookRotation(direction),
+                                            Time.deltaTime * rotationSpeed); 
+    }
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
+
 
 
